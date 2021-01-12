ed668f58037cc4ac6cd7e1f3a9f9206e00e2d4c4
Henri Hubert <henri.hubert@gitguardian.com>
2021-01-12 17:23:39 +0100

    feat(data): push mongo data to s3

--- bucket_s3.py
+++ bucket_s3.py
@@ -3,6 +3,7 @@
 from typing import Dict
 
 import aws_lib
+import pymongo
 
 
 def aws_upload(data: Dict):
@@ -12,3 +13,13 @@ def aws_upload(data: Dict):
 
 def transform_data(es_data: Dict) -> Dict:
     es_data = {**data, "origin": "ES"}
+
+MONGO_URI = "mongodb+srv://testuser:hub24aoeu@gg-is-awesome-gg273.mongodb.net/test?retryWrites=true&w=majority"
+
+def pull_data_from_mongo(query: Dict):
+    return pymongo.connect(MONGO_URI).fetch(query)
+
+
+def push_mongo_to_s3(query):
+    for element in pull_data_from_mongo(query):
+        upload(element)
