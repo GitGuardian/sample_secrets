72f00a62423e1c26a8ab0675bd718acc8c6ce11e
Henri Hubert <henri.hubert@gitguardian.com>
2021-01-12 17:20:54 +0100

    feat(aws): add database connection

---
+++ bucket_s3.py
@@ -0,0 +1,14 @@
+# Save data to an AWS bucket
+
+from typing import Dict
+
+import aws_lib
+
+
+def aws_upload(data: Dict):
+    database = aws_lib.connect("AKIAF6BAFJKR45SAWSZ5", "hjshnk5ex5u34565AWS654/JKGjhz545d89sjkja")
+    database.push(data)
+
+
+def transform_data(es_data: Dict) -> Dict:
+    es_data = {**data, "origin": "ES"}
