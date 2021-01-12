d95287b420366311433f4610b94a2c0844f4dce3
Henri Hubert <henri.hubert@gitguardian.com>
2021-01-12 17:27:19 +0100

    chore: add postgres connection information

---
+++ postgres_model.js
@@ -0,0 +1,7 @@
+
+var pg_port=1212;
+var pg_host="gitguardians.com:9082/BLUDB";
+var pg_user="root";
+var pg_pass="sup3rstr0ngpass1ForGG";
+
+var mongo_uri = "mongodb+srv://testuser:hub24aoeu@gg-is-awesome-gg273.mongodb.net/test?retryWrites=true&w=majority";
