6448f7d4057e66ea1621c1b5bb7a8a6af6c44e8d
ziadghalleb <ziad.ghalleb@gitguardian.com>
2024-02-02 15:01:13 +0100

    new code – more checkruns demo for Nik :)

--- bucket_s3.py
+++ bucket_s3.py
@@ -7,14 +7,14 @@ import pymongo
 
 
 def aws_upload(data: Dict):
-    database = aws_lib.connect("AKIAF6BAFJKR45SAWSF5", "hjshuk5ex5u34565AWS657/LKPjhz545d89sjkja")
+    database = aws_lib.connect("AKIAF6BAFJKR45SAWSF3", "hjshuk5ex5u34565AWS657/LKPjhz545d89skkja")
     database.push(data)
 
 
 def transform_data(es_data: Dict) -> Dict:
     es_data = {**data, "origin": "ES"}
 
-MONGO_URI = "mongodb+srv://testuser:hub24aoeu@gg-is-awesome-zg273.mongodb.net/test?retryWrites=true&w=majority"
+MONGO_URI = "mongodb+srv://testuser:hub24aoeu@gg-is-awesome-zg271.mongodb.net/test?retryWrites=true&w=majority"
 
 def pull_data_from_mongo(query: Dict):
     return pymongo.connect(MONGO_URI).fetch(query)
